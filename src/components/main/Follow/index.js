a79540138500c9fcd65d8ea536e9511daa0669d8
orozaliev0 <orozaliev.pp@gmail.com>
2023-04-17 11:26:46 +0600

    back

--- src/components/main/Follow/index.js
+++ src/components/main/Follow/index.js
@@ -3,6 +3,7 @@ import '../../../style/Follow.scss'
 import {useNavigate} from "react-router-dom";
 import more1 from '../../../img/more1.svg'
 import more from '../../../img/more.svg'
+import transition from "react-transition-group/Transition";
 
 const Follow = () => {
 
@@ -10,6 +11,10 @@ const Follow = () => {
     const follow =()=>{
         navigate("/followPage")
         window.scroll(0,0)
+    }
+  const more =()=>{
+        navigate("/more")
+        window.scroll(0,0,)
     }
 
     return (
@@ -24,7 +29,7 @@ const Follow = () => {
                     <div className='more__btn'>
                         <button onClick={() => follow()} className='more__btn--btn1'>Подписаться
                         </button>
-                        <button onClick={() => navigate('/more')} className='more__btn--btn2'>Узнать больше</button>
+                        <button onClick={() => more()} className='more__btn--btn2'>Узнать больше</button>
                     </div>
                     <img src={more} alt="" className='more__img2'/>
                 </div>
