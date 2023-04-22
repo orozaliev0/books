31fc9cb96c9d2fb64dcd51ab31ec4427a26f6aa6
Malikadevv <malikajyrgalbaeva@gmail.com>
2023-04-22 19:17:41 +0600

    jwrhewjrhfdbjdty vuewrygbcdvsaeituejdvndfjhgeurktbjgjertyuytrgbvnfdtubfhetguethgfbnfgghjsdgjsfjdshguterbffvndhdtgnvdhguretrihgjjegbetuergjvnvejjdjryrejenvjdftuejfjetuiegnvsbnasgruyewtugbjdghcvm,nfvgerytudfnngdjeytndghuejnndghgsfng  dtjhtejg erk yuer eirtyt ditybvnfge etg sdjrbf run  nejoy jeth ve pment nwetg

---
+++ src/pages/restoration/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import imageBook from "../../img/coco.svg";
+
+const Restoration = () => {
+    return (
+        <div>
+            <div className="w-full h-full  z-50 fixed account">
+                <div className="flex justify-center items-center flex-col">
+                    <img src={imageBook} alt=""/>
+                    <h1 className="h">SELF DEVELOPING SCHOOL</h1>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Restoration;
